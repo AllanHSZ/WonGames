122ab2c42def356cf3d527267345398049755cbd
Allan <allanhsz@outlook.com>
2021-06-05 19:33:07 -0300

    FEATURE: Criando componente helper MediaMatch

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,17 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
   addons: ['@storybook/addon-essentials'],
+  // react-docgen-typescript error on yarn storybook
+  // https://github.com/styleguidist/react-docgen-typescript/issues/356
+  typescript: {
+    reactDocgen: 'none'
+  },
   babel: async (options) => ({
     ...options,
-    plugins: [...options.plugins,  require.resolve('@babel/plugin-transform-react-jsx')]
+    plugins: [
+      ...options.plugins,
+      require.resolve('@babel/plugin-transform-react-jsx')
+    ]
   }),
   webpackFinal: (config) => {
     config.resolve.modules.push(`${process.cwd()}/src`)
