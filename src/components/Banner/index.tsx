9414a620e878e243172d243871720236a81fff3a
Allan <allanhsz@outlook.com>
2021-06-11 20:53:57 -0300

    FEATURE: Implementando Ribbon mo Banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,4 +1,5 @@
 import Button from 'components/Button'
+import Ribbon, { RibbonSizes, RibbonColors } from 'components/Ribbon'
 
 import * as S from './styles'
 
@@ -8,16 +9,28 @@ export type BannerProps = {
   subtitle: string
   buttonLabel: string
   buttonLink: string
+  ribbon?: string
+  ribbonSize?: RibbonSizes
+  ribbonColor?: RibbonColors
 }
 
 const Banner = ({
   img,
   title,
   subtitle,
+  ribbon,
+  ribbonSize,
+  ribbonColor,
   buttonLabel,
   buttonLink
 }: BannerProps) => (
   <S.Wrapper>
+    {!!ribbon && (
+      <Ribbon color={ribbonColor} size={ribbonSize}>
+        {ribbon}
+      </Ribbon>
+    )}
+
     <S.Image src={img} role="img" aria-label={title} />
     <S.Caption>
       <S.Title>{title}</S.Title>
