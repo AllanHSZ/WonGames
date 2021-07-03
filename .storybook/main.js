fc1e21c27c91c28dd68f718ecb73ca5cc702eb6b
Allan Henrique <allanhsz@outlook.com>
2021-07-03 02:07:18 -0300

    CHORE: Storybook atualizando versao a reabilitando reactDocgen

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,6 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
   addons: ['@storybook/addon-essentials'],
-  // react-docgen-typescript error on yarn storybook
-  // https://github.com/styleguidist/react-docgen-typescript/issues/356
-  typescript: {
-    reactDocgen: 'none'
-  },
   babel: async (options) => ({
     ...options,
     plugins: [
