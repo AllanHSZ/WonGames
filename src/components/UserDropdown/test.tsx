0577c6787bd622f391f2a0f6d2c06533a3e464d5
Allan Henrique <allanhsz@outlook.com>
2021-08-08 20:49:53 -0300

    FEATURE: Criando componente UserDropdown

---
+++ src/components/UserDropdown/test.tsx
@@ -0,0 +1,27 @@
+import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import UserDropdown from '.'
+
+describe('<UserDropdown />', () => {
+  it('should render the username', () => {
+    renderWithTheme(<UserDropdown username="Allan" />)
+
+    expect(screen.getByText(/allan/i)).toBeInTheDocument()
+  })
+
+  it('should render the menu', () => {
+    renderWithTheme(<UserDropdown username="Allan" />)
+
+    // open menu
+    userEvent.click(screen.getByText(/allan/i))
+
+    expect(
+      screen.getByRole('link', { name: /my profile/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('link', { name: /wishlist/i })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /sign out/i })).toBeInTheDocument()
+  })
+})
