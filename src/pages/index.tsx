acd40844d6ae0d8ca46577f91b11584721b4a3b2
Allan Henrique <allanhsz@outlook.com>
2021-08-29 00:27:40 -0300

    FIX: Home e Game, ajustando relavidate e cache

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,12 +16,13 @@ export async function getStaticProps() {
     data: { banners, newGames, upcomingGames, freeGames, sections }
   } = await apolloClient.query<QueryHome, QueryHomeVariables>({
     query: QUERY_HOME,
-    variables: { date: TODAY }
+    variables: { date: TODAY },
+    fetchPolicy: 'no-cache' // Nao busca no cache e nem salve, sempre ira fazer uma nova requisicao
   })
 
   return {
+    revalidate: 10,
     props: {
-      revalidate: 10,
       banners: bannerMapper(banners),
       newGamesTitle: sections?.newGames?.title,
       newGames: gamesMapper(newGames),
