d193fccf014a75aab59ced7084a5f25f7cd205a7
Allan Henrique <allanhsz@outlook.com>
2021-10-03 01:36:22 -0300

    FEATURE: GameItem adicionando opcao de remover do carrinho

--- src/components/GameItem/test.tsx
+++ src/components/GameItem/test.tsx
@@ -1,8 +1,11 @@
+import userEvent from '@testing-library/user-event'
+import { CartContextDefaultValues } from 'hooks/use-cart'
 import { render, screen } from 'utils/test-utils'
 
 import GameItem from '.'
 
 const props = {
+  id: '1',
   img: 'https://source.unsplash.com/user/willianjusten/151x70',
   title: 'Red Dead Redemption 2',
   price: 'R$ 215,00'
@@ -15,11 +18,28 @@ describe('<GameItem />', () => {
     expect(
       screen.getByRole('heading', { name: props.title })
     ).toBeInTheDocument()
+
     expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
       'src',
       props.img
     )
-    expect(screen.getByText(props.price)).toBeInTheDocument()
+
+    expect(screen.getByText('R$ 215,00')).toBeInTheDocument()
+  })
+
+  it('should render remove if the item is inside the cart and call remove', () => {
+    const cartProviderProps = {
+      ...CartContextDefaultValues,
+      isInCart: () => true,
+      removeFromCart: jest.fn()
+    }
+    render(<GameItem {...props} />, { cartProviderProps })
+
+    const removeLink = screen.getByText(/remove/i)
+    expect(removeLink).toBeInTheDocument()
+
+    userEvent.click(removeLink)
+    expect(cartProviderProps.removeFromCart).toHaveBeenCalledWith('1')
   })
 
   it('should render the item with download link', () => {
@@ -35,10 +55,11 @@ describe('<GameItem />', () => {
   it('should render the payment info', () => {
     const paymentInfo = {
       flag: 'mastercard',
-      img: '/img/cards/mastercard.png',
+      img: '/img/master-card.png',
       number: '**** **** **** 4326',
       purchaseDate: 'Purchase made on 07/20/2020 at 20:32'
     }
+
     render(<GameItem {...props} paymentInfo={paymentInfo} />)
 
     expect(screen.getByRole('img', { name: paymentInfo.flag })).toHaveAttribute(
