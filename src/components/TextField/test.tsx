174824b0829bf0e1b60fa553472237d8ed2e2d23
Allan Henrique <allanhsz@outlook.com>
2021-10-07 00:41:13 -0300

    REFACTOR: TextField alterando parametro onInput para onInputChange

--- src/components/TextField/test.tsx
+++ src/components/TextField/test.tsx
@@ -1,8 +1,6 @@
-import { waitFor } from '@testing-library/react'
+import { render, screen, waitFor } from 'utils/test-utils'
 import userEvent from '@testing-library/user-event'
 import { Email } from '@styled-icons/material-outlined'
-
-import { render, screen } from 'utils/test-utils'
 
 import TextField from '.'
 
@@ -40,8 +38,14 @@ describe('<TextField />', () => {
   })
 
   it('Changes its value when typing', async () => {
-    const onInput = jest.fn()
-    render(<TextField onInput={onInput} label="TextField" name="TextField" />)
+    const onInputChange = jest.fn()
+    render(
+      <TextField
+        onInputChange={onInputChange}
+        label="TextField"
+        name="TextField"
+      />
+    )
 
     const input = screen.getByRole('textbox')
     const text = 'This is my new text'
@@ -49,16 +53,16 @@ describe('<TextField />', () => {
 
     await waitFor(() => {
       expect(input).toHaveValue(text)
-      expect(onInput).toHaveBeenCalledTimes(text.length)
+      expect(onInputChange).toHaveBeenCalledTimes(text.length)
     })
-    expect(onInput).toHaveBeenCalledWith(text)
+    expect(onInputChange).toHaveBeenCalledWith(text)
   })
 
   it('Does not changes its value when disabled', async () => {
-    const onInput = jest.fn()
+    const onInputChange = jest.fn()
     render(
       <TextField
-        onInput={onInput}
+        onInputChange={onInputChange}
         label="TextField"
         name="TextField"
         disabled
@@ -74,7 +78,7 @@ describe('<TextField />', () => {
     await waitFor(() => {
       expect(input).not.toHaveValue(text)
     })
-    expect(onInput).not.toHaveBeenCalled()
+    expect(onInputChange).not.toHaveBeenCalled()
   })
 
   it('Renders with error', () => {
