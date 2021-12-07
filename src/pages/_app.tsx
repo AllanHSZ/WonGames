06dc223c4ac2b5a80c99a642251d18aec7838734
Allan Henrique <allanhsz@outlook.com>
2021-12-07 16:48:24 -0300

    FEATURE: Criando hook useWishlist

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,7 @@ import NextNprogress from 'nextjs-progressbar'
 
 import { useApollo } from 'utils/apollo'
 import { ThemeProvider } from 'styled-components'
+import { WishlistProvider } from 'hooks/use-wishlist'
 import { CartProvider } from 'hooks/use-cart'
 
 import GlobalStyles from 'styles/global'
@@ -19,24 +20,26 @@ function App({ Component, pageProps }: AppProps) {
       <ApolloProvider client={client}>
         <ThemeProvider theme={theme}>
           <CartProvider>
-            <Head>
-              <title>Won Games</title>
-              <link rel="shortcut icon" href="/img/icon-512.png" />
-              <link rel="apple-touch-icon" href="/img/icon-512.png" />
-              <link rel="manifest" href="/manifest.json" />
-              <meta
-                name="description"
-                content="The best Game Stores in the world!"
+            <WishlistProvider>
+              <Head>
+                <title>Won Games</title>
+                <link rel="shortcut icon" href="/img/icon-512.png" />
+                <link rel="apple-touch-icon" href="/img/icon-512.png" />
+                <link rel="manifest" href="/manifest.json" />
+                <meta
+                  name="description"
+                  content="The best Game Stores in the world!"
+                />
+              </Head>
+              <GlobalStyles />
+              <NextNprogress
+                color="#F231A5"
+                startPosition={0.3}
+                stopDelayMs={200}
+                height={5}
               />
-            </Head>
-            <GlobalStyles />
-            <NextNprogress
-              color="#F231A5"
-              startPosition={0.3}
-              stopDelayMs={200}
-              height={5}
-            />
-            <Component {...pageProps} />
+              <Component {...pageProps} />
+            </WishlistProvider>
           </CartProvider>
         </ThemeProvider>
       </ApolloProvider>
