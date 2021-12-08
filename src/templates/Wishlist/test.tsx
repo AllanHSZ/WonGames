80ba46fa858422ba8a5a155a3068fbde6954af7b
Allan Henrique <allanhsz@outlook.com>
2021-12-07 22:51:23 -0300

    FEATURE: Wishlist integrando com a api

--- src/templates/Wishlist/test.tsx
+++ src/templates/Wishlist/test.tsx
@@ -2,15 +2,15 @@ import 'session.mock'
 import 'match-media-mock'
 import { render, screen } from 'utils/test-utils'
 
-import Wishlist, { WishlistTemplateProps } from '.'
+import Wishlist from '.'
 
 import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
+import { WishlistContextDefaultValues } from 'hooks/use-wishlist'
 
-const props: WishlistTemplateProps = {
-  games: gamesMock,
-  recommendedHighlight: highlightMock,
+const props = {
   recommendedTitle: 'You may like these games',
+  recommendedHighlight: highlightMock,
   recommendedGames: gamesMock
 }
 
@@ -30,26 +30,38 @@ jest.mock('components/Showcase', () => ({
 
 describe('<Wishlist />', () => {
   it('should render correctly', () => {
-    render(<Wishlist {...props} />)
+    const wishlistProviderProps = {
+      ...WishlistContextDefaultValues,
+      items: [gamesMock[0]]
+    }
+
+    render(<Wishlist {...props} />, { wishlistProviderProps })
 
     expect(
       screen.getByRole('heading', { name: /wishlist/i })
     ).toBeInTheDocument()
 
-    expect(screen.getAllByText(/population zero/i)).toHaveLength(6)
+    expect(screen.getByText(/population zero/i)).toBeInTheDocument()
     expect(screen.getByTestId('Mock Showcase')).toBeInTheDocument()
   })
 
   it('should render empty when there are no games', () => {
+    const wishlistProviderProps = {
+      ...WishlistContextDefaultValues,
+      items: []
+    }
+
     render(
       <Wishlist
-        recommendedTitle={props.recommendedTitle}
+        recommendedTitle="You may like these games"
         recommendedGames={gamesMock}
         recommendedHighlight={highlightMock}
-      />
+      />,
+      { wishlistProviderProps }
     )
 
     expect(screen.queryByText(/population zero/i)).not.toBeInTheDocument()
+
     expect(
       screen.getByRole('heading', { name: /your wishlist is empty/i })
     ).toBeInTheDocument()
