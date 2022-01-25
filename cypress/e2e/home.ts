48951c0e0d80233f356f20de1e3dab7d56ed5f43
Allan Henrique <allanhsz@outlook.com>
2022-01-25 14:39:41 -0300

    TEST: Home adicionando testes e2e

--- cypress/e2e/home.ts
+++ cypress/e2e/home.ts
@@ -4,5 +4,11 @@ describe('Home Page', () => {
     it('should render home sections', () => {
         // visita a pagina
         cy.visit('/')
+
+        cy.shouldRenderBanner()
+        cy.shouldRenderShowcase({ name: "New Games", highlight: false })
+        cy.shouldRenderShowcase({ name: "Most Popular Games", highlight: true })
+        cy.shouldRenderShowcase({ name: "Upcoming Games", highlight: true })
+        cy.shouldRenderShowcase({ name: "Free Games", highlight: true })
     })
 })
--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,12 +1,35 @@
 // load type definitions from Cypress module
 /// <reference types="cypress" />
 
+type ShowcaseAttributes = {
+  name: string
+  highlight?: boolean
+}
+
 declare namespace Cypress {
   interface Chainable {
     /**
-     * Custom command to visit google page
-     * @example cy.google()
-     */
+    * Custom command to visit google page
+    * @example cy.google()
+    */
     google(): Chainable<Window>
+
+    /**
+    * Custom command to get element by data-cy
+    * @example cy.getByDataCy('selector')
+    */
+    getByDataCy(selector: string): Chainable<Element>
+
+    /**
+    * Custom command to check banner in page
+    * @example cy.shouldRenderBanner()
+    */
+    shouldRenderBanner(): Chainable<Element>
+
+    /**
+    * Custom command to check banner in page
+    * @example cy.shouldRenderShowcase()
+    */
+    shouldRenderShowcase(attrs: ShowcaseAttributes): Chainable<Element>
   }
 }
