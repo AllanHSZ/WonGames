a3c149f715c15e7c22130b4565ff0d77cc47cd0e
Allan Henrique <allanhsz@outlook.com>
2022-04-09 18:21:08 -0300

    TEST: SignIn adicionando testes e2e

--- cypress/e2e/user.ts
+++ cypress/e2e/user.ts
@@ -11,4 +11,15 @@ describe('User', () => {
     cy.url().should('eq', `${Cypress.config().baseUrl}/`)
     cy.findByText(user.username).should('exist')
   });
+
+  it('should sign in and sign out', () => {
+    cy.visit('/sign-in')
+    cy.signIn()
+
+    cy.findByText(/cypress/i).should('exist').click()
+    cy.findByText(/sign out/i).click()
+
+    cy.findByRole('link', { name: /sign in/i }).should('exist')
+    cy.findByText(/cypress/i).should('not.exist')
+  });
 });
